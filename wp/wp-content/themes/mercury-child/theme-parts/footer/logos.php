d1814195d0dce2c0d20e8ddef60c89ef4dece3ec
Dmitry Krapivko <dmitry.krapivko@legalbet.com>
2023-08-16 12:52:43 +0200

    added links on logos

--- wp/wp-content/themes/mercury-child/theme-parts/footer/logos.php
+++ wp/wp-content/themes/mercury-child/theme-parts/footer/logos.php
@@ -3,9 +3,9 @@
     <div class="logo"> <a href="http://www.gamcare.org.uk/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/gamcare.png" alt="gamcare logo"></a></div>
     <div class="logo"> <a href="https://www.begambleaware.org/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/gambleaware.png" alt="gambleaware logo"></a></div>
     <div class="logo"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/18plus.png" alt="18 plus logo"></div>
-    <div class="logo"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/ecogra.png" alt="ecogra logo"></div>
-    <div class="logo"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/gpwa.png" alt="gpwa logo"></div>
-    <div class="logo"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/trustarc.png" alt="trustarc logo"></div>
-    <div class="logo"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/dmca.png" alt="dmca logo"></div>
-    <div class="logo"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/nortonlifelock.png" alt="nortonlifelock logo"></div>
+    <div class="logo"> <a href="https://ecogra.org/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/ecogra.png" alt="ecogra logo"></a></div>
+    <div class="logo"> <a href="https://www.gpwa.org/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/gpwa.png" alt="gpwa logo"></a></div>
+    <div class="logo"> <a href="https://trustarc.com/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/trustarc.png" alt="trustarc logo"></a></div>
+    <div class="logo"> <a href="https://www.dmca.com/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/dmca.png" alt="dmca logo"></a></div>
+    <div class="logo"> <a href="https://www.nortonlifelock.com/es/es/" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/footer/nortonlifelock.png" alt="nortonlifelock logo"></a></div>
 </div>
