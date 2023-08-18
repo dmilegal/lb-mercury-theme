2af8319a4ae82036a051c46aeaea1c8c3921f5b9
Dmitry Krapivko <dmitry.krapivko@legalbet.com>
2023-08-18 12:41:18 +0200

    feat: add separately person schema

--- wp/wp-content/themes/mercury-child/inc/filters/structured-data/structured-data.php
+++ wp/wp-content/themes/mercury-child/inc/filters/structured-data/structured-data.php
@@ -1,3 +1,4 @@
 <?php
 require_once 'author.php';
 require_once 'review.php';
+require_once 'person-static.php';
--- wp/wp-content/themes/mercury-child/settings/yoast/schema/schema.php
+++ wp/wp-content/themes/mercury-child/settings/yoast/schema/schema.php
@@ -1,2 +1,3 @@
 <?php
 require_once 'review.php';
+require_once 'person-static.php';
