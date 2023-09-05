d5540028cdd55073a9f57c6cba05ef7f2a25ab95
dmilegal <dmitry.krapivko@legalbet.com>
2023-09-05 21:10:01 +0200

    fix styles order problems & right assets connection

--- wp/wp-content/themes/mercury-child/functions.php
+++ wp/wp-content/themes/mercury-child/functions.php
@@ -14,8 +14,18 @@ require_once 'inc/inc.php';
 // For custom functions and hooks...
 function include_child_scripts()
 {
-    wp_register_style('child-styles', get_stylesheet_directory_uri() . '/css/child-styles.css', 'all');
-    wp_enqueue_style('child-styles');
+    wp_deregister_style('mercury-style');
+    wp_deregister_style('mercury-media');
+    wp_deregister_style('mercury-googlefonts');
+
+    wp_enqueue_style( 'mercury-googlefonts', get_stylesheet_directory_uri() . '/fonts/stylesheet.css', array(), '20230829', 'all' );
+
+    wp_enqueue_style( 'mercury-style', get_template_directory_uri() . '/style.css', array(), '20230829', 'all' );
+    wp_enqueue_style( 'mercury-child-style', get_stylesheet_directory_uri() . '/css/child-styles.css', array('mercury-style'), '20230829', 'all' );
+    wp_enqueue_style( 'mercury-media', get_template_directory_uri() . '/css/media.css',  array('mercury-child-style'), '20230829', 'all');
+    #wp_enqueue_style( 'mercury-child-media', get_stylesheet_directory_uri() . '/css/child-media.css',  array('mercury-child-style'), '20230829', 'all');
+
+    wp_enqueue_script( 'jquery-fix', get_stylesheet_directory_uri() . '/js/libs/jquery-fix.js', array() );
     wp_enqueue_script('child-scripts', get_stylesheet_directory_uri() . '/js/child-scripts.js', array('jquery'));
 }
 add_action('wp_enqueue_scripts', 'include_child_scripts', 25);
