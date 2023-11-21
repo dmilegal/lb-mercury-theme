f9d0ac59a51c6efec0f0bd5ef8242926b5413988
dmilegal <dmitry.krapivko@legalbet.com>
2023-11-21 22:44:43 +0000

    increase  url exists cache time

--- wp/wp-content/themes/mercury-child/shared/check-url-exists.php
+++ wp/wp-content/themes/mercury-child/shared/check-url-exists.php
@@ -10,7 +10,7 @@ function check_url_exists($url) {
     $response = wp_remote_head($url);
     $result = !is_wp_error($response) && wp_remote_retrieve_response_code($response) == 200;
 
-    set_site_transient($cache_key, $result ? 1 : 0, DAY_IN_SECONDS);
+    set_site_transient($cache_key, $result ? 1 : 0, 4 * WEEK_IN_SECONDS);
 
     return $result;
 }
