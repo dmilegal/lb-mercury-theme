4445a6f05470b4ffffab5019a6d1d13bd267087a
dmilegal <dmitry.krapivko@legalbet.com>
2023-12-08 22:21:04 +0300

    footer logos svg

--- wp/wp-content/themes/mercury-child/theme-parts/footer/logos.php
+++ wp/wp-content/themes/mercury-child/theme-parts/footer/logos.php
@@ -1,13 +1,19 @@
 <?
 $logos_arr = get_field('sett_footer_logos','option');
 $logos_html = '';
-
-if(is_array($logos_arr) && count($logos_arr)>0):
-	$logos_html .= '<div class="footer-logos">';
+if(is_array($logos_arr) && count($logos_arr)>0):?>
+	<div class="footer-logos"><?
 		foreach ($logos_arr as $item):
-		
-			$img = wp_get_attachment_image($item['img'], 'thumbnail');
-			$logos_html .= '<div class="logo">';
+			if(!$item['size'])
+				$item['size']='thumbnail';
+
+			$img = wp_get_attachment_image($item['img'], $item['size']);
+			if($item['boxed']){
+				$logos_html .= '<div class="logo boxed" style="width:'.$item["width"].'px;">';
+			}
+			else{
+				$logos_html .= '<div class="logo ">';
+			}
 			$rel = ($item['rel'] !== '' ? 'rel="'.$item['rel'].'"' : '');
 			if($item['url'] !== '')
 				$logos_html .= '<a href="'.$item['url'].'" '.$rel.' '.($item['blank'] ? "target='_blank'" : "").'>';
@@ -16,6 +22,7 @@ if(is_array($logos_arr) && count($logos_arr)>0):
 			$logos_html .= ($item['url'] !== '' ? '</a>' : '');
 			$logos_html .= '</div>';	
 		endforeach;
-	$logos_html .= '</div>';	
-	echo $logos_html;
+	echo $logos_html;?>
+
+	</div><?
 endif;
