2b3a1c010c6cdee7bd1e4cfeac8874dd2c2b03b5
dmilegal <dmitry.krapivko@legalbet.com>
2023-12-23 20:47:02 +0300

    up

--- wp/wp-content/themes/mercury-child/single.php
+++ wp/wp-content/themes/mercury-child/single.php
@@ -4,7 +4,7 @@
 get_template_part('theme-parts/molecules/hero-banner', null, [
 	'title' => get_the_title(),
 	'subtitle' => 'Lucy Bond is an interior designer who started her career in New Zealand, working for large architectural firms. We chatted to her about design and starting her own studio.',
-	'published_date' => get_the_date(),
+	'published_date' => get_the_date('Y-m-d'),
 ]);
 ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
--- wp/wp-content/themes/mercury-child/theme-parts/organisms/sidebar/sidebar.php
+++ wp/wp-content/themes/mercury-child/theme-parts/organisms/sidebar/sidebar.php
@@ -1,4 +1,10 @@
-<div class="lb-sidebar">
-	
-	<?= get_template_part('theme-parts/molecules/toc'); ?>
+<div class="lb-sidebar not-prose">
+	<div class="lb-sidebar__block">
+		<div class="lb-sidebar__block-title">
+			<?= __('Table of contents', 'mercury-child') ?>
+		</div>
+		<div class="lb-sidebar__block-content">
+			<?= get_template_part('theme-parts/molecules/toc'); ?>
+		</div>
 	</div>
+</div>
