b42cb4dc60093f6c2608683e194c32f7bb5ce825
dmilegal <dmitry.krapivko@legalbet.com>
2024-01-12 22:30:29 +0300

    up

--- wp/wp-content/themes/mercury-child/theme-parts/molecules/promo-bonus.php
+++ wp/wp-content/themes/mercury-child/theme-parts/molecules/promo-bonus.php
@@ -8,5 +8,29 @@ $style = $args['style'] ?? '';
             ) ?>" style="<?= stylesValue(
                             $style
                           ) ?>">
-  <div></div>
+  <div class="lb-promo-bonus__title">
+    Bonus PokerStars Casino
+  </div>
+  <div  class="lb-promo-bonus__subtitle">
+    Up to £100 Extra Winnings on Your First Sport Bet
+  </div>
+  <div  class="lb-promo-bonus__actions">
+    <?
+    get_template_part('theme-parts/atoms/button', null, [
+      'size' => 'xl',
+      'variant' => 'outlined',
+      'content' => __('Copy Promo', 'mercury-child'),
+      'prefix' => '<i class="icon-copy"></i>',
+      'className' => 'lb-promo-bonus__copy'
+    ]);
+    get_template_part('theme-parts/atoms/button', null, [
+      'size' => 'xl',
+      'color' => 'primary',
+      'content' => __('Get bonus', 'mercury-child'),
+    ]);
+    ?>
+  </div>
+  <div class="lb-promo-bonus__tc">
+    Read about <a>T&C</a>
+  </div>
 </div>
