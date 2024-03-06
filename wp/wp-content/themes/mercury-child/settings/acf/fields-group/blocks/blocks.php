d57c36f21ed025cb1a591f8188c61c7b39eeb826
dmilegal <dmitry.krapivko@legalbet.com>
2024-03-06 09:50:53 +0300

    fix: add acf group for sidebar block

--- wp/wp-content/themes/mercury-child/settings/acf/fields-group/blocks/blocks.php
+++ wp/wp-content/themes/mercury-child/settings/acf/fields-group/blocks/blocks.php
@@ -4,3 +4,4 @@ require_once 'quote.php';
 require_once 'casino-list.php';
 require_once 'step-list.php';
 require_once 'button.php';
+require_once 'sidebar-block.php';
---
+++ wp/wp-content/themes/mercury-child/settings/acf/fields-group/blocks/sidebar-block.php
@@ -0,0 +1,51 @@
+<?
+add_action('acf/include_fields', function () {
+  if (!function_exists('acf_add_local_field_group')) {
+    return;
+  }
+
+  acf_add_local_field_group(array(
+    'key' => 'group_65e77c2bdfaa8',
+    'title' => 'Sidebar Block',
+    'fields' => array(
+      array(
+        'key' => 'field_65e77c2ce715a',
+        'label' => 'Bordered',
+        'name' => 'is_bordered',
+        'aria-label' => '',
+        'type' => 'true_false',
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'message' => '',
+        'default_value' => 1,
+        'ui' => 0,
+        'ui_on_text' => '',
+        'ui_off_text' => '',
+      ),
+    ),
+    'location' => array(
+      array(
+        array(
+          'param' => 'block',
+          'operator' => '==',
+          'value' => 'lb/sidebar-block',
+        ),
+      ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+    'show_in_rest' => 0,
+  ));
+});
