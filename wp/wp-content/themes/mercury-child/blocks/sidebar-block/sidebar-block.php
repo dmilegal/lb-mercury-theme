c8700a661a878e36052b1b7afc2b3613b6a8f4e8
dmilegal <dmitry.krapivko@legalbet.com>
2024-03-13 00:29:49 +0300

    up

--- wp/wp-content/themes/mercury-child/blocks/sidebar-block/sidebar-block.php
+++ wp/wp-content/themes/mercury-child/blocks/sidebar-block/sidebar-block.php
@@ -13,7 +13,7 @@
  */
 
 $allowed_blocks = array('lb/sidebar-block-title', 'core/paragraph', 
-'core/heading', 'lb/contributor-list', 'lb/share-links', 'lb/toc');
+'core/heading', 'lb/contributor-list', 'lb/share-links', 'lb/toc', 'lb/casino-list');
 $template = array(
   array('lb/sidebar-block-title')
 );
@@ -22,6 +22,7 @@ $isBordered = get_field('is_bordered');
 ?>
 <div class="<?= classNames("lb-sidebar-block", [
               'lb-sidebar-block--bordered' => $isBordered,
+              'lb-sidebar-block--preview' => $is_preview,
             ])  ?>">
   <InnerBlocks templateLock="false" allowedBlocks="<?= esc_attr(wp_json_encode($allowed_blocks)) ?>" template="<?= esc_attr(wp_json_encode($template)) ?>" />
 </div>
