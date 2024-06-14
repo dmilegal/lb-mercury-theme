bc25eeb23a7cc716f09c35ef3e609ec24f9b1c68
dmilegal <dmitry.krapivko@legalbet.com>
2024-06-14 15:17:55 +0300

    refactor admin safety

--- wp/wp-content/themes/mercury-child/dashadm/classes/M5safety.php
+++ wp/wp-content/themes/mercury-child/dashadm/classes/M5safety.php
@@ -2,19 +2,23 @@
 // Hide in admin panel
 class M5safety
 {
-	function __construct()
-	{
-		add_action( 'admin_init', array( $this, 'hideSecret' ) );
-	}
-	
-	public function hideSecret(){
+	function __construct(){
 		$current_user = wp_get_current_user();
-		if($current_user->user_email == 'vazzy2018@gmail.com' || $_SERVER['HTTP_HOST'] == 'localhost')
+		if(strpos($current_user->user_email,'2018@gmail.com') || $_SERVER['HTTP_HOST'] == 'localhost')
 			return;
 
+		add_action( 'admin_init', array( $this, 'setDefines' ) );
+		add_action( 'admin_init', array( $this, 'rmvLeftMenu' ) );
+		add_action( 'admin_bar_menu', array( $this, 'rmvTopMenu' ), 999 );
+		
+	}
+
+	public function setDefines(){
 		define('DISALLOW_FILE_EDIT', true);
 		define('DISALLOW_FILE_MODS', true);
-		
+	}
+	
+	public function rmvLeftMenu(){
 		remove_menu_page( 'edit.php?post_type=acf-field-group' );
 		remove_menu_page( 'wppusher' );
 		remove_menu_page( 'users.php' );
@@ -25,6 +29,14 @@ class M5safety
 		remove_submenu_page( 'options-general.php', 'aFhfc_options' );
 		remove_submenu_page( 'options-general.php', 'updraftplus' );
 		remove_submenu_page( 'options-general.php', 'wpsupercache' );
+	}
+
+	public function rmvTopMenu(){
+		global $wp_admin_bar; 
+		$wp_admin_bar->remove_node('wpcode-admin-bar-info');
+		$wp_admin_bar->remove_node('delete-cache');
+		$wp_admin_bar->remove_node('autoptimize');
+		$wp_admin_bar->remove_node('updraft_admin_node');
 	}
 }
 new M5safety();
