ec7e89e7e25d0c7fe43749f99159e2f2bd7c4690
dmilegal <dmitry.krapivko@legalbet.com>
2024-06-22 21:09:43 +0300

    up

--- wp/wp-content/themes/mercury-child/frontend/dist/js/blocks/blocks.js
+++ wp/wp-content/themes/mercury-child/frontend/dist/js/blocks/blocks.js
@@ -647,6 +647,41 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _cookie_banner_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./cookie-banner.scss */ "./src/blocks/cookie-banner/cookie-banner.scss");
 
+(function () {
+  const body = document.body;
+  const content = document.getElementById('cmplz-cookiebanner-container');
+  const list = ['age-restriction'];
+  if (!content) return;
+  function checkAgeRestriction() {
+    if (list.some(cl => body.classList.contains(cl))) {
+      // Если класс есть, элемент должен быть скрыт дольше
+      body.classList.add('cb-hide');
+
+      // Начинаем наблюдение за body
+      observer.observe(body, observerOptions);
+    }
+  }
+
+  // Обработчик для наблюдения за изменениями классов на body
+  const observer = new MutationObserver(() => {
+    if (!list.some(cl => body.classList.contains(cl))) {
+      // Если классы удалены, запускаем анимацию через 4с
+      setTimeout(() => {
+        body.classList.remove('cb-hide');
+      }, 4000);
+      observer.disconnect();
+    }
+  });
+
+  // Опции для наблюдения за изменениями атрибутов (классов)
+  const observerOptions = {
+    attributes: true,
+    attributeFilter: ['class']
+  };
+
+  // Проверяем начальное состояние
+  checkAgeRestriction();
+})();
 
 /***/ }),
 
