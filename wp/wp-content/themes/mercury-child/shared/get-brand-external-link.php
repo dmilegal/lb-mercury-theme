8320d0c011580d72b985618198d05edd5f279507
dmilegal <dmitry.krapivko@legalbet.com>
2024-07-02 14:43:30 +0300

    fix ref

--- wp/wp-content/themes/mercury-child/shared/get-brand-external-link.php
+++ wp/wp-content/themes/mercury-child/shared/get-brand-external-link.php
@@ -4,6 +4,7 @@ function getBrandExternalLink($brandId)
   $bonusId = aces_get_main_casino_bonus_id($brandId);
   $casino_external_link = esc_url(get_post_meta($brandId, 'casino_external_link', true));
   $bonus_external_link = esc_url(get_post_meta($bonusId, 'bonus_external_link', true));
+  $brand_permalink = get_permalink($brandId);
 
   $external_link_url = "";
 
@@ -11,6 +12,8 @@ function getBrandExternalLink($brandId)
     $external_link_url = $bonus_external_link;
   } elseif ($casino_external_link) {
     $external_link_url = $casino_external_link;
+  } elseif (!get_field('enable_modal_ref_list', 'option')) {
+    $external_link_url = $brand_permalink;
   }
 
   return $external_link_url;
