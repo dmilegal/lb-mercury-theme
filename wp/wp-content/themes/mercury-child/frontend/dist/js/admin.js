2396287800a394a0b7f5eb3d8bbfa5d830653870
dmilegal <dmitry.krapivko@legalbet.com>
2024-11-14 20:49:26 +0300

    up

--- wp/wp-content/themes/mercury-child/frontend/dist/js/admin.js
+++ wp/wp-content/themes/mercury-child/frontend/dist/js/admin.js
@@ -1,1 +1,134 @@
-var e={80:()=>{var e;e=jQuery,acf&&acf.add_filter("select2_ajax_data",(function(a,r,d,l,t){var i,n;if("field_668d9b8bce1ad_block"===(null===(i=t.data)||void 0===i||null===(i=i.field)||void 0===i?void 0:i.get("key"))||"field_668d9b8bce1ad_tpl"===(null===(n=t.data)||void 0===n||null===(n=n.field)||void 0===n?void 0:n.get("key"))){var o=acf.findFields({key:"field_670ea17e4269d_block",sibling:l});if(o.length||(o=acf.findFields({key:"field_670ea17e4269d_tpl",sibling:l})),!o.length)return;var f=e(o[0]).data("acf");if(!f)return;var v=f.val();a.bonus=v||""}return a}))}},a={};function r(d){var l=a[d];if(void 0!==l)return l.exports;var t=a[d]={exports:{}};return e[d](t,t.exports,r),t.exports}r.n=e=>{var a=e&&e.__esModule?()=>e.default:()=>e;return r.d(a,{a}),a},r.d=(e,a)=>{for(var d in a)r.o(a,d)&&!r.o(e,d)&&Object.defineProperty(e,d,{enumerable:!0,get:a[d]})},r.o=(e,a)=>Object.prototype.hasOwnProperty.call(e,a),r(80);
+/******/ var __webpack_modules__ = ({
+
+/***/ "./public/fonts/lhicon/stylesheet.css":
+/*!********************************************!*\
+  !*** ./public/fonts/lhicon/stylesheet.css ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/admin/listing-bonus-category-bonus.ts":
+/*!***************************************************!*\
+  !*** ./src/admin/listing-bonus-category-bonus.ts ***!
+  \***************************************************/
+/***/ (() => {
+
+(function ($) {
+  if (!acf) return;
+  // Добавляем фильтр для модификации данных AJAX-запроса select2
+  acf.add_filter('select2_ajax_data', function (data, args, $input, field, instance) {
+    var _instance$data, _instance$data2;
+    // Проверяем, что это поле 'bonus_category' (замените на ваш ключ поля)
+    if (((_instance$data = instance.data) === null || _instance$data === void 0 || (_instance$data = _instance$data.field) === null || _instance$data === void 0 ? void 0 : _instance$data.get('key')) === 'field_668d9b8bce1ad_block' || ((_instance$data2 = instance.data) === null || _instance$data2 === void 0 || (_instance$data2 = _instance$data2.field) === null || _instance$data2 === void 0 ? void 0 : _instance$data2.get('key')) === 'field_668d9b8bce1ad_tpl') {
+      var bonusEls = acf.findFields({
+        key: 'field_670ea17e4269d_block',
+        sibling: field
+      });
+      if (!bonusEls.length) {
+        bonusEls = acf.findFields({
+          key: 'field_670ea17e4269d_tpl',
+          sibling: field
+        });
+      }
+      if (!bonusEls.length) return;
+
+      // Получаем значение из поля 'bonus' (замените на ваш ключ поля)
+      var bonusField = $(bonusEls[0]).data('acf');
+      if (!bonusField) return;
+      var bonusValue = bonusField.val();
+      // Добавляем параметр 'bonus' в данные AJAX-запроса
+      data.bonus = bonusValue || '';
+    }
+
+    // Возвращаем измененные данные
+    return data;
+  });
+})(jQuery);
+
+/***/ })
+
+/******/ });
+/************************************************************************/
+/******/ // The module cache
+/******/ var __webpack_module_cache__ = {};
+/******/ 
+/******/ // The require function
+/******/ function __webpack_require__(moduleId) {
+/******/ 	// Check if module is in cache
+/******/ 	var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 	if (cachedModule !== undefined) {
+/******/ 		return cachedModule.exports;
+/******/ 	}
+/******/ 	// Create a new module (and put it into the cache)
+/******/ 	var module = __webpack_module_cache__[moduleId] = {
+/******/ 		// no module.id needed
+/******/ 		// no module.loaded needed
+/******/ 		exports: {}
+/******/ 	};
+/******/ 
+/******/ 	// Execute the module function
+/******/ 	__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 
+/******/ 	// Return the exports of the module
+/******/ 	return module.exports;
+/******/ }
+/******/ 
+/************************************************************************/
+/******/ /* webpack/runtime/compat get default export */
+/******/ (() => {
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = (module) => {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			() => (module['default']) :
+/******/ 			() => (module);
+/******/ 		__webpack_require__.d(getter, { a: getter });
+/******/ 		return getter;
+/******/ 	};
+/******/ })();
+/******/ 
+/******/ /* webpack/runtime/define property getters */
+/******/ (() => {
+/******/ 	// define getter functions for harmony exports
+/******/ 	__webpack_require__.d = (exports, definition) => {
+/******/ 		for(var key in definition) {
+/******/ 			if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 				Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 			}
+/******/ 		}
+/******/ 	};
+/******/ })();
+/******/ 
+/******/ /* webpack/runtime/hasOwnProperty shorthand */
+/******/ (() => {
+/******/ 	__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ })();
+/******/ 
+/******/ /* webpack/runtime/make namespace object */
+/******/ (() => {
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = (exports) => {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/ })();
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+/*!****************************!*\
+  !*** ./src/admin/admin.ts ***!
+  \****************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var fonts_lhicon_stylesheet_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! fonts/lhicon/stylesheet.css */ "./public/fonts/lhicon/stylesheet.css");
+/* harmony import */ var _listing_bonus_category_bonus__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./listing-bonus-category-bonus */ "./src/admin/listing-bonus-category-bonus.ts");
+/* harmony import */ var _listing_bonus_category_bonus__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_listing_bonus_category_bonus__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+//# sourceMappingURL=admin.js.map
