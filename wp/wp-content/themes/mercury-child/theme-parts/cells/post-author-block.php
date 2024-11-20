c4299f0476fdc2d728d451cacc13f26406b58819
dmilegal <dmitry.krapivko@legalbet.com>
2024-11-20 21:08:57 +0300

    up

--- wp/wp-content/themes/mercury-child/theme-parts/cells/post-author-block.php
+++ wp/wp-content/themes/mercury-child/theme-parts/cells/post-author-block.php
@@ -18,13 +18,14 @@ if (empty($display_name))
       <?= esc_html($displayName) ?>
     </span>
     <?php if (!get_theme_mod('mercury_date_display')) { ?>
-      <span class="lb-post-author__pub-date">
+      <time class="lb-post-author__pub-date" datetime="<?= get_the_date('Y-m-d', $postId) ?>" pubdate="pubdate">
         <?php if (get_theme_mod('mercury_time_ago_format')) {
         ?>
           <?php printf(esc_html_x('%s ago', '%s = human-readable time difference', 'mercury-child'), human_time_diff(get_the_time('U', $postId), current_time('timestamp'))); ?>
         <?php } else {
           echo get_the_date('', $postId);
-        } ?></span>
+        } ?>
+      </time>
     <?php } ?>
   </div>
 </div>
