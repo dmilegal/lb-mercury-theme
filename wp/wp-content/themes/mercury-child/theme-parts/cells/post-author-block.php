e25f67206972b1d62f9b515c16e9bacc707226b9
dmilegal <dmitry.krapivko@legalbet.com>
2024-11-20 18:28:17 +0300

    up

---
+++ wp/wp-content/themes/mercury-child/theme-parts/cells/post-author-block.php
@@ -0,0 +1,30 @@
+<?
+$postId =  $args['post_id'] ?? null;
+$authorId = get_post_field('post_author', $post_id);
+$user = get_userdata($authorId);
+
+$displayName = $user->display_name;
+
+if (empty($display_name))
+  $displayName = $user->nickname;
+?>
+<div class="lb-post-author">
+  <?= get_avatar($authorId, 32, '', '', [
+    'class' => 'lb-post-author__avatar',
+    'loading' => 'lazy'
+  ]) ?>
+  <div class="lb-post-author__info">
+    <span class="lb-post-author__name">
+      <?= esc_html($displayName) ?>
+    </span>
+    <?php if (!get_theme_mod('mercury_date_display')) { ?>
+      <span class="lb-post-author__pub-date">
+        <?php if (get_theme_mod('mercury_time_ago_format')) {
+        ?>
+          <?php printf(esc_html_x('%s ago', '%s = human-readable time difference', 'mercury-child'), human_time_diff(get_the_time('U', $postId), current_time('timestamp'))); ?>
+        <?php } else {
+          echo get_the_date('', $postId);
+        } ?></span>
+    <?php } ?>
+  </div>
+</div>
