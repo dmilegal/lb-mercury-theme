803ddb0fe5aa08c7ed364d8098f36f70176bfad2
dmilegal <dmitry.krapivko@legalbet.com>
2025-01-29 23:01:44 +0300

    up

---
+++ wp/wp-content/themes/mercury-child/frontend/src/blocks/trust-info-block/trust-info-block.ts
@@ -0,0 +1,1 @@
+import './trust-info-block.scss'
---
+++ wp/wp-content/themes/mercury-child/theme-parts/cells/trust-info-block.php
@@ -0,0 +1,13 @@
+<div class="lb-trust-info-block not-prose">
+  <div class="lb-trust-info-block__inner">
+  <header class="lb-trust-info-block__header">
+    <h2 class="lb-trust-info-block__title">Why you should trust</h2>
+  </header>
+  <ul class="lb-trust-info-block__list">
+    <li class="lb-trust-info-block__item" data-title="1000+ Hours of Testing">
+      <img width="40" height="40" >
+    </li>
+  </ul>
+  </div>
+ 
+</div>
